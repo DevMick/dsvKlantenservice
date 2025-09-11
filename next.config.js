01500fcc87a3bd5b8abd08e746106ffba1fc03c0
DevMick <devmick@example.com>
2025-09-11 17:16:31 +0000

    revert(maintenance): remove /maintenance page and disable root redirect

--- next.config.js
+++ next.config.js
@@ -3,15 +3,6 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com'],
   },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/maintenance',
-        permanent: false,
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
